cd6a523b8f5359610e697dcc9febf51ba7c9c555
Jarno van Wijgerden <Jarno@marvelous.nl>
2024-01-31 10:33:45 +0100

    Get al properties from configuration

--- App_Plugins/OrderChildrenByProperty/Controllers/OrderByChildrenApiController.cs
+++ App_Plugins/OrderChildrenByProperty/Controllers/OrderByChildrenApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Web.BackOffice.Controllers;
 using Umbraco13StudieProject.App_Plugins.OrderChildrenByProperty.Editor;
@@ -10,31 +11,35 @@ namespace Umbraco13StudieProject.App_Plugins.OrderChildrenByProperty.Controllers
     {
         public ActionResult GetAllPropertiesOfChildren(int currentPageId)
         {
-            var children = contentService.GetPagedChildren(currentPageId, 0, 9999, out _);
-
-            if (children != null && children.Any())
+            var currentPage = contentService.GetById(currentPageId);
+            var currentPageProp = currentPage?.Properties.FirstOrDefault(x => x.PropertyType.PropertyEditorAlias == Global.OrderChildrenByPropertyAlias);
+            if (currentPageProp != null)
             {
-                var currentPage = contentService.GetById(currentPageId);
-                var currentPageProp = currentPage.Properties.FirstOrDefault(x => x.PropertyType.PropertyEditorAlias == Global.OrderChildrenByPropertyAlias);
                 var dt = dataTypeService.GetDataType(currentPageProp.PropertyType.DataTypeId);
-                var config = dt.ConfigurationAs<OrderChildrenByPropertyConfiguration>();
-                var properties = config.Properties; //object is JToken. Set deze data naar eigen object en lees dit uit
+                var config = dt?.ConfigurationAs<OrderChildrenByPropertyConfiguration>();
+                var jToken = config?.Properties as JToken;
+                var dic = new Dictionary<string, string>();
 
-                ////var pageTypes = children.Count(x => x.ContentType)
-
-                //////todo checken wat we doen als er meerdere pageTypes zijn. Misschien iets van configuratie hiervoor maken?
-                ////var properties = 
-
-                var dic = new Dictionary<string, string>
+                if (jToken == null)
                 {
-                    { "Name", "Name" },
-                    { "CreateDate", "Create date" },
-                    { "PublishDate", "Publish date" }
-                };
+                    return new JsonResult("No properties found");
+                }
+
+                foreach (var o in jToken.Children<JObject>())
+                {
+                    foreach (var p in o.Properties())
+                    {
+                        var value = (string)p.Value;
+                        if (!dic.Values.Contains(value))
+                        {
+                            dic.Add(value, value);
+                        }
+                    }
+                }
 
                 return new JsonResult(dic);
             }
-            return new JsonResult("No children found");
+            return new JsonResult("No properties found");
         }
     }
 }
